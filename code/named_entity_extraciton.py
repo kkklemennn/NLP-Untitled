da071935890836eec07d254e59c9632748d92759
kkklemennn <klemen.klemar@hotmail.com>
2022-05-02 20:52:58 +0200

    Performance analysis and small fixes

--- code/named_entity_extraciton.py
+++ code/named_entity_extraciton.py
@@ -5,6 +5,8 @@ import pandas as pd
 import numpy as np
 import stanza
 import spacy_stanza
+import timeit
+import json
 from nltk.corpus import stopwords
 
 #%%
@@ -21,11 +23,11 @@ def remove_multiple_spaces(token):
 	return " ".join(token.split())
 
 def clean_entity(token):
-	toclean = ['\'s']
-	for el in toclean:
-		if el in token:
-			token = token.replace(el, '')
-	return remove_multiple_spaces(token)
+    toclean = ['\'s', 'the']
+    for el in toclean:
+        if el in token.lower():
+            token = token.lower().replace(el, '')
+    return remove_multiple_spaces(token)
 
 def get_entities_with_spacy(sentence):
 	doc = ner(text)
@@ -58,49 +60,50 @@ def get_common_entities_spacy(text):
 	return occurence_list 
 
 def get_common_entities_stanza(text):
-	doc = ner_spacy_stanza_tagger(text)
-	#all_entities = doc.ents
-	all_entities =  []
-	# for token in doc:
-	# 	if token.ent_type_ == "PERSON":
-	# 		all_entities.append(token.text)
-	# 	print(token.text, token.lemma_, token.pos_, token.dep_, token.ent_type_)
+    doc = ner_spacy_stanza_tagger(text)
+    #all_entities = doc.ents
+    all_entities =  []
+    # for token in doc:
+    #     if token.ent_type_ == "PERSON":
+    #         all_entities.append(token.text)
+    #     print(token.text, token.lemma_, token.pos_, token.dep_, token.ent_type_)
 
-	for index, token in enumerate(doc):
-		# print(index, token.text, token.ent_type_)
-		if token.ent_type_ == "PERSON":
-			current_person = []
-			current_person.append(token.text)
-			idx = index + 1
-			next = doc[idx]
-			while next.ent_type_ == "PERSON":
-				current_person.append(next.text)
-				idx += 1
-				next = doc[idx]
-			current_person = ' '.join([str(item) for item in current_person])
-			current_person = clean_entity(current_person)
+    for index, token in enumerate(doc):
+        # print(index, token.text, token.ent_type_)
+        stopwords = ["and", "a"]
+        if token.ent_type_ == "PERSON" and token.text.lower() not in stopwords:
+            current_person = []
+            current_person.append(token.text)
+            idx = index + 1
+            next = doc[idx]
+            while next.ent_type_ == "PERSON" and next.text.lower() not in stopwords:
+                current_person.append(next.text)
+                idx += 1
+                next = doc[idx]
+            current_person = ' '.join([str(item) for item in current_person])
+            current_person = clean_entity(current_person)
 
-			# Check if substring is in list
-			flag = False
-			for el in all_entities:
-				if current_person in el:
-					flag = True
-			if not flag:
-				all_entities.append(current_person)
+            # Check if substring is in list
+            flag = False
+            for el in all_entities:
+                if current_person in el:
+                    flag = True
+            if not flag:
+                all_entities.append(current_person)
 
-	#print("Unprocessed -> ", all_entities)
-	# Transform Span object to list, make lowercase
-	filtered_list = [entity.lower() for entity in all_entities]
-	# Unify occurences
-	filtered_list = list(dict.fromkeys(filtered_list))
-	#...
+    #print("Unprocessed -> ", all_entities)
+    # Transform Span object to list, make lowercase
+    filtered_list = [entity.lower() for entity in all_entities]
+    # Unify occurences
+    filtered_list = list(dict.fromkeys(filtered_list))
+    #...
 
-	return filtered_list
+    return filtered_list
 
 #%%
 def get_book_string(file_path):
-	with open(file_path, 'r') as file:
-		return file.read().rstrip()
+    with open(file_path, 'r', encoding='utf8') as file:
+        return file.read().rstrip()
 	
 
 # %% 
@@ -117,8 +120,49 @@ if (novel is None):
 
 print("file string ->", novel[0:50])
 #named_entities = get_common_entities_spacy(novel)
-print(get_common_entities_stanza(novel))
+# print(get_common_entities_stanza(novel))
 #stops = set(stopwords.words('english'))
 #print(stops)
 
+# %%
+# =========== PERFORMANCE ANALYSIS ===========
+def get_book_string(file_path):
+	with open(file_path, 'r', encoding='utf8') as file:
+		return file.read().rstrip()
+
+short_stories_path = "../material/short_stories_corpus/"
+
+files = os.listdir(short_stories_path)
+
+
+def get_performance(model):
+    performance_model = {}
+    for file in files:
+        print(file)
+        performance = {}
+        novel = get_book_string(short_stories_path + file)
+        start = timeit.default_timer()
+        if model == 'stanza':
+            entities = get_common_entities_stanza(novel)
+        elif model == 'spacy':
+            entities = get_common_entities_spacy(novel)
+        else:
+            print("Enter either stanza or spacy")
+            exit()
+        occurences = {}
+        for entity in entities:
+            occurences[entity] = novel.lower().count(entity)
+        stop = timeit.default_timer()
+        performance['entities'] = occurences
+        performance['runtime'] = stop - start
+        performance_model[file] = performance
+        print(performance)
+        # break
+    return performance_model
+
+performance_stanza = get_performance('stanza')
+# print(performance_stanza)
+
+with open("performance_stanza", "w+", encoding="utf-8") as outfile:
+    json.dump(performance_stanza, outfile, indent=4, ensure_ascii=False)
 # %%
